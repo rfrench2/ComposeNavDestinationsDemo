1c4a85f3cd6401d36f53c3e3acc107d71926bfca
rfrench2 <rfrench2@gmail.com>
2023-06-01 07:42:07 -0400

    Changes so project would build.

--- app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
+++ app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.plcoding.composenavdestinationsdemo
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -8,30 +9,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.plcoding.composenavdestinationsdemo.destinations.PostScreenDestination
 import com.plcoding.composenavdestinationsdemo.destinations.ProfileScreenDestination
 import com.plcoding.composenavdestinationsdemo.ui.theme.ComposeNavDestinationsDemoTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import java.time.Instant
+import org.w3c.dom.Text
 import java.time.LocalDateTime
-import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
